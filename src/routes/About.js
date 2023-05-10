a7db02fbf264c33f9f152b07dea0a8f0fa4c8f18
Dominick Salazar <dominicksalazar788@yahoo.com>
2023-05-10 12:55:41 -0700

    add technologies

--- src/routes/About.js
+++ src/routes/About.js
@@ -3,12 +3,14 @@ import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
 import HeroImg2 from '../components/HeroImg2'
 import AboutContent from '../components/AboutContent'
+import Technologies from '../components/Technologies'
 
 const About = () => {
   return (
     <div>
      <Navbar/>
      <HeroImg2 heading="ABOUT." text="im a friendly Front-end Developer"/>
+     <Technologies />
      <AboutContent />
      <Footer/>
     </div>
