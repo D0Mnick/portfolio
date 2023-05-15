25c3937f9775779a70e9712996fcf1a7c025701e
Dominick Salazar <dominicksalazar788@yahoo.com>
2023-05-15 12:06:24 -0700

    footer changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -15,7 +15,7 @@ const Footer = () => {
                     </div>
                 </div>
                 <div className="phone">
-                    <h4> <FaPhone  size={20} style={{color:"fff", marginRight:"2rem"}}/>09957657605</h4>
+                    <h4> <FaPhone  size={20} style={{color:"fff", marginRight:"2rem"}}/>09957657606</h4>
                 </div>
                 
                 <div className="email">
